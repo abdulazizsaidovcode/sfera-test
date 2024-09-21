9566f76e8cf07e3e9ccfa1010d66ea3a9b08b893
PABLO <temirovmuhammadyusuf70@gmail.com>
2024-09-21 12:59:04 +0500

    aaaaa

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,6 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import './css/style.css';
 import './css/satoshi.css';
-import 'jsvectormap/dist/css/jsvectormap.css';
-import 'flatpickr/dist/flatpickr.min.css';
-import 'react-lazy-load-image-component/src/effects/blur.css';
 import { Toaster } from 'react-hot-toast';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
